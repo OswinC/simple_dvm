16008e85da60ddf9a5f132f7f9439a175e8b4cec
Ramax Lo <ramax.lo@vivotek.com>
2014-05-29 20:32:56 +0800

    Add hash_table header

---
+++ simple_dvm/hash_table.h
@@ -0,0 +1,14 @@
+#ifndef _HASHTABLE_H_
+#define _HASHTABLE_H_
+
+struct hash_table
+{
+	struct list_head entries[128];
+};
+
+void hash_init(struct hash_table *table);
+int hash(char *ptr);
+void hash_add(struct hash_table *table, struct list_head *node, int key);
+struct list_head *hash_get(struct hash_table *table, int key);
+
+#endif // _HASHTABLE_H_
